72053f009bfdfe77ec6ac6e64fa38d9126d64683
Aedonpt <81579466+Aedonpt@users.noreply.github.com>
2021-04-20 11:56:59 +0100

    Update setup.sh

--- BitwardenRS/setup.sh
+++ BitwardenRS/setup.sh
@@ -69,6 +69,7 @@ sh <(curl -sSL https://get.docker.com) &>/dev/null
 #  portainer/portainer-ce &>/dev/null
 
 # Install Portainer Agent
+msg "Installing PortainerAgent"
 apt-get update && apt-get upgrade \
 curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
 chmod +x /usr/local/bin/docker-compose \

b18e332fbd27102316a95f5ef236fe604d04b96d
Aedonpt <81579466+Aedonpt@users.noreply.github.com>
2021-04-20 12:11:40 +0100

    Update setup.sh

--- BitwardenRS/setup.sh
+++ BitwardenRS/setup.sh
@@ -53,6 +53,9 @@ EOF
 # Install Docker
 msg "Installing Docker..."
 sh <(curl -sSL https://get.docker.com) &>/dev/null
+docker swarm init &>/dev/null
+docker stack deploy --compose-file=/agent-stack.yml portainer-agent &>/dev/null
+docker service ls &>/dev/null
 
 # Install Portainer
 #msg "Installing Portainer..."
@@ -70,25 +73,6 @@ sh <(curl -sSL https://get.docker.com) &>/dev/null
 
 # install Portainer Agent
 msg "Installing PortainerAgent"
-docker network create --driver overlay --attachable portainer_agent_network
-
-docker service create \
-    --name portainer_agent \
-    --network portainer_agent_network \
-    --mode global \
-    --constraint 'node.platform.os == linux' \
-    --mount type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock \
-    --mount type=bind,src=//var/lib/docker/volumes,dst=/var/lib/docker/volumes \
-    portainer/agent
-	
-docker service create \
-    --name portainer \
-    --network portainer_agent_network \
-    --publish 9000:9000 \
-    --publish 8000:8000 \
-    --replicas=1 \
-    --constraint 'node.role == manager' \
-    portainer/portainer -H "tcp://tasks.portainer_agent:9001" --tlsskipverify
 
 # Install Watchtower
 msg "Installing Watchtower..."

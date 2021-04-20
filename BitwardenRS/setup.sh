da036b2ced78802832f31c0b6622443c5a165581
Aedonpt <81579466+Aedonpt@users.noreply.github.com>
2021-04-20 12:00:57 +0100

    Update setup.sh

--- BitwardenRS/setup.sh
+++ BitwardenRS/setup.sh
@@ -68,15 +68,6 @@ sh <(curl -sSL https://get.docker.com) &>/dev/null
 #  -v /docker/portainer:/data \
 #  portainer/portainer-ce &>/dev/null
 
-# Install Portainer Agent
-msg "Installing PortainerAgent"
-apt-get update && apt-get upgrade \
-curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
-chmod +x /usr/local/bin/docker-compose \
-docker swarm init; curl -L https://downloads.portainer.io/agent-stack.yml -o agent-stack.yml; \
-docker stack deploy --compose-file=agent-stack.yml portainer-agent; \
-docker service ls
-
 # Install Watchtower
 msg "Installing Watchtower..."
 docker run -d \

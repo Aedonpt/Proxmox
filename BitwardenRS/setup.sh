714321187160af972faac50c479c23e9853c1913
Aedonpt <81579466+Aedonpt@users.noreply.github.com>
2021-04-20 12:03:20 +0100

    Update setup.sh

--- BitwardenRS/setup.sh
+++ BitwardenRS/setup.sh
@@ -68,6 +68,28 @@ sh <(curl -sSL https://get.docker.com) &>/dev/null
 #  -v /docker/portainer:/data \
 #  portainer/portainer-ce &>/dev/null
 
+# install Portainer Agent
+msg "Installing PortainerAgent"
+docker network create --driver overlay --attachable portainer_agent_network
+
+docker service create \
+    --name portainer_agent \
+    --network portainer_agent_network \
+    --mode global \
+    --constraint 'node.platform.os == linux' \
+    --mount type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock \
+    --mount type=bind,src=//var/lib/docker/volumes,dst=/var/lib/docker/volumes \
+    portainer/agent
+	
+docker service create \
+    --name portainer \
+    --network portainer_agent_network \
+    --publish 9000:9000 \
+    --publish 8000:8000 \
+    --replicas=1 \
+    --constraint 'node.role == manager' \
+    portainer/portainer -H "tcp://tasks.portainer_agent:9001" --tlsskipverify
+
 # Install Watchtower
 msg "Installing Watchtower..."
 docker run -d \
